ddea5af89955f8c5fdfb87672f7dd40f478f1846
fernando ubiratan <fernandomcp23@gmail.com>
2022-08-14 00:05:14 -0300

    Modulo Conta Corrente com construtc, get, setter e refatorado.

---
+++ CursoJavaBasico/src/com/fernando/cursoOO/exercicio28_33/ContaCorrente/TesteContaCorrente.java
@@ -0,0 +1,85 @@
+package com.fernando.cursoOO.exercicio28_33.ContaCorrente;
+import java.util.Scanner;
+
+public class TesteContaCorrente {
+
+	public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in);
+			
+			ContaCorrente contaF = new ContaCorrente(45856, 100.00, 1000.00,  0);
+			contaF.setNumero(22225555);
+			contaF.setSaldo(500.98);
+			contaF.setLimite(2000.00);
+						
+			boolean sair = false;
+			int opcoes;
+			double valor;
+			int contDeposito = 0;
+			double valorLimiteInicial = contaF.getLimite();
+
+			while (!sair) {
+				System.out.println("" + "  ___   _ __    ___  _ __   __ _   ___  ___\n"
+						+ " / _ \\ | '_ \\  / _ \\| '__| / _` | / _ \\/ __|\n"
+						+ "| (_) || |_) ||  __/| |   | (_| ||  __/\\__ \\\n"
+						+ " \\___/ | .__/  \\___||_|    \\__,_| \\___||___/\n" + "       | |\n" + "       |_|");
+				System.out.println("Escolha dentre as opções abaixo:");
+				System.out.println("1.Saque");
+				System.out.println("2.Deposito");
+				System.out.println("3.Extrato");
+				System.out.println("4.Sair");
+				opcoes = scan.nextInt();
+
+				if (opcoes == 1) {
+					String resp;
+					System.out.println("Informe o valor do saque.");
+					valor = scan.nextDouble();
+					boolean flagLimite = false;
+					boolean sacandoValor;
+
+					while (!flagLimite) {
+						if (valor > 0) {
+							sacandoValor = contaF.sacar(valor);
+							if (!sacandoValor) {
+								System.out.println("Saldo Insuficiente. Gostaria de sacar do seu limite de R$"
+										+ contaF.getLimite() + " ? S/N");
+								resp = scan.next();
+								flagLimite = contaF.verificarUtilizacaoLimite(valor, resp);
+							} else {
+								contaF.usarChequeEspecial(false);
+								System.out.println("Operação realizada com sucesso. Retire seu dinheiro.");
+								flagLimite = true;
+							}
+
+						} else {
+							System.out.println("Valor de saque inválido! Tente novamente.");
+							flagLimite = true;
+						}
+
+					}
+					flagLimite = true;
+
+				} else if (opcoes == 2) {
+					System.out.println("Informe o valor do depósito.");
+					valor = scan.nextDouble();
+
+					contaF.depositarEmSaldoELimite(valor, valorLimiteInicial);
+					contaF.Especial(valor, contDeposito);
+					contDeposito++;
+					
+				} else if (opcoes == 3) {
+					boolean validarLimite = contaF.getLimite() != valorLimiteInicial;
+					contaF.usarChequeEspecial(validarLimite ? true : false);
+					contaF.mostrarExtrato();
+					
+				} else if (opcoes == 4) {
+					sair = true;
+				} else {
+					System.out.println("Opção inexistente.");
+				}
+
+			}
+
+		}
+
+	}
+
