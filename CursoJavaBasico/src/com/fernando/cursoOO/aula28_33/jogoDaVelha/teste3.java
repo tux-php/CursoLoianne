3175cf8a46fcaefc3669769e43d4e62d19998299
tux-php <fernandomcp23@gmail.com>
2022-12-01 22:22:09 -0300

    teste3

--- CursoJavaBasico/src/com/fernando/cursoOO/aula28_33/jogoDaVelha/teste3.java
+++ CursoJavaBasico/src/com/fernando/cursoOO/aula28_33/jogoDaVelha/teste3.java
@@ -3,7 +3,7 @@ package com.fernando.cursoOO.aula28_33.jogoDaVelha;
 public class teste3 {
 
 	public static void main(String[] args) {
-		System.out.println("teste");
+		// TODO Auto-generated method stub
 
 	}
 
