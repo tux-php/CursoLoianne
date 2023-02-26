ddfe60237fd16ad3f16e5d82ab93d573ad08874b
tux-php <fernandomcp23@gmail.com>
2023-02-25 21:37:08 -0300

    Classes Agenda, Contato e Teste Refatorados e corrigidos.

--- CursoJavaBasico/src/com/fernando/cursoOO/aula36/lab01/TesteAgendaContato.java
+++ CursoJavaBasico/src/com/fernando/cursoOO/aula36/lab01/TesteAgendaContato.java
@@ -6,6 +6,7 @@
 package com.fernando.cursoOO.aula36.lab01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -24,7 +25,7 @@ public class TesteAgendaContato {
         while (!sair)
         {
             opcao = false;
-            System.out.println("Iniciar o cadastro Agenda (S/N) ?");
+            System.out.println("Iniciar o cadastro da nova Agenda (S/N) ?");
             String resp = scan.next();
             if (resp.equalsIgnoreCase("s"))
             {
@@ -37,11 +38,11 @@ public class TesteAgendaContato {
 
                     if (agenda != null && agenda.getNome() != null)
                     {
-                        String[] contatoNome = new String[2];
-                        String[] contatoTelefone = new String[2];
-                        String[] contatoEmail = new String[2];
+                        String[] contatoNome = new String[4];
+                        String[] contatoTelefone = new String[4];
+                        String[] contatoEmail = new String[4];
                         //Contato contato = new Contato();
-                        Contato contato[] = new Contato[2];
+                        Contato contato[] = new Contato[3];
 
                         for (int i = 1; i < contato.length; i++)
                         {
@@ -68,72 +69,29 @@ public class TesteAgendaContato {
 
                         }
 
-                    System.out.println("Nome Agenda: " + agenda.getNome());
-                    if (agenda != null && agenda.getContatos() != null)
-                    {
-                        //List<Contato> teste = (List<Contato>) agenda.getContatos();
-                        //for (Object listaContato : agenda.getContatos())
-                        //{
-                            System.out.println("Contato nome: " + agenda.getContatos().get(0));
-                            System.out.println("Contato telefone: " + agenda.getContatos().get(1));
-                            System.out.println("Contato email: " + agenda.getContatos().get(2));
-                        //}
-                    }
-                    opcao = true;
+                        //System.out.println("Nome Agenda: " + agenda.getNome());
+                        if (agenda != null && agenda.getContatos() != null)
+                        {
+
+                            for (Object listaContato : agenda.getContatos())
+                            {
+                                System.out.println("Contato: " + listaContato);
+                            }
+
+                        }
+                        opcao = true;
                     }
                     System.out.println("Gostaria de refazer a operação ? (S/N)");
                     String reiniciar = scan.next();
 
                     opcao = reiniciar.equalsIgnoreCase("s") ? false : true;
                 }
-//                            System.out.println("Digite o nome 1º contato:");
-//                            String contatoNome1 = scan.next();
-//                            System.out.println("Digite o telefone 1º contato:");
-//                            String contatoTelefone1 = scan.next();
-//                            System.out.println("Digite o email 1º contato:");
-//                            String contatoEmail1 = scan.next();
-//
-//                            Contato contato1 = new Contato();
-//
-//                            contato1.setNome(contatoNome1);
-//                            contato1.setTelefone(contatoTelefone1);
-//                            contato1.setEmail(contatoEmail1);
-//
-//                            System.out.println("Digite o nome 2º contato:");
-//                            String contatoNome2 = scan.next();
-//                            System.out.println("Digite o telefone 2º contato:");
-//                            String contatoTelefone2 = scan.next();
-//                            System.out.println("Digite o email 2º contato:");
-//                            String contatoEmail2 = scan.next();
-//
-//                            Contato contato2 = new Contato();
-//                            contato2.setNome(contatoNome2);
-//                            contato2.setTelefone(contatoTelefone2);
-//                            contato2.setEmail(contatoEmail2);
-//
-//                            //Contato[] contatos = new Contato[2];
-//                            contatos[0] = contato1;
-//                            contatos[1] = contato2;
-//                            agenda.setContatos(contatos);
-//
-//                            System.out.println("Nome Agenda: " + agenda.getNome());
-//
-//                            if (agenda != null && agenda.getContatos() != null)
-//                            {
-//                                for (Contato c : agenda.getContatos())
-//                                {
-//                                    System.out.println("Contato nome: " + c.getNome());
-//                                    System.out.println("Contato telefone: " + c.getTelefone());
-//                                    System.out.println("Contato email: " + c.getEmail());
-//                                }
-//                            }
-//                            opcao = true;
-//                        }
-//                        System.out.println("Gostaria de refazer a operação ? (S/N)");
-//                        String reiniciar = scan.next();
-//
-//                        opcao = reiniciar.equalsIgnoreCase("s") ? false : true;
+
             }
+            System.out.println("Gostaria de sair da aplicação ? (S/N)");
+            String resp_sair = scan.next();
+
+            sair = resp_sair.equalsIgnoreCase("s") ? true : false;
         }
         sair = true;
     }
